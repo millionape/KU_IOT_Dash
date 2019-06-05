1aec59562df241a015475534269dade8d0ba5286
millionape <kittaporn.jp@gmail.com>
2019-06-05 18:18:11 +0700

    add ajax at home

---
+++ public/js/homeUpdatedTime.js
@@ -0,0 +1,46 @@
+function initHome() {
+    (function poll() {
+        setTimeout(function () {
+            $.ajax({
+                url: "/dash/getdata?gid=83&id=31",
+                success: function (data) {
+                    try {
+                        var thTime = data[0].dt.toLocaleString("en-US", {timeZone: "asia/bangkok"});
+                        $('#update2').text(new Date(thTime));
+                        $('#online2').css("color","green")
+                    }catch(err){
+                        console.log("ERROR from home ajax.")
+                    }
+                },
+                error: function () {
+                    console.log('ERROR')
+                },
+                dataType: "json",
+                type: 'GET',
+                complete: poll
+            });
+        }, 1000);
+    })();
+    (function poll() {
+        setTimeout(function () {
+            $.ajax({
+                url: "/dash/getdata?gid=84&id=41",
+                success: function (data) {
+                    try {
+                        var thTime = data[0].dt.toLocaleString("en-US", {timeZone: "asia/bangkok"});
+                        $('#update3').text(new Date(thTime));
+                        $('#online3').css("color","green")
+                    }catch(err){
+                        console.log("ERROR from home ajax.")
+                    }
+                },
+                error: function () {
+                    console.log('ERROR')
+                },
+                dataType: "json",
+                type: 'GET',
+                complete: poll
+            });
+        }, 1000);
+    })();
+}
