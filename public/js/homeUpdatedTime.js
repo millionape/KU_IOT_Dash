e9da573a59143215f230d1434e0105f01116b933
millionape <kittaporn.jp@gmail.com>
2019-06-06 18:37:32 +0700

    add gateway 85

--- public/js/homeUpdatedTime.js
+++ public/js/homeUpdatedTime.js
@@ -43,4 +43,26 @@ function initHome() {
             });
         }, 1000);
     })();
+    (function poll() {
+        setTimeout(function () {
+            $.ajax({
+                url: "/dash/getdata?gid=85&id=51",
+                success: function (data) {
+                    try {
+                        var thTime = data[0].dt.toLocaleString("en-US", {timeZone: "asia/bangkok"});
+                        $('#update4').text(new Date(thTime));
+                        $('#online4').css("color","green")
+                    }catch(err){
+                        console.log("ERROR from home ajax.")
+                    }
+                },
+                error: function () {
+                    console.log('ERROR')
+                },
+                dataType: "json",
+                type: 'GET',
+                complete: poll
+            });
+        }, 1000);
+    })();
 }
