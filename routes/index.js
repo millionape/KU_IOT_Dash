acfd7878664c76a510bd364fcd3ff5f9767b5ae9
millionape <kittaporn.jp@gmail.com>
2019-08-05 21:53:14 +0700

    kjo

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req ,res) {
   var _gid = 55
   var _id = 51
   try{
-    request('http://119.59.116.183:8080/values?id='+_id31+'&gid='+_gid83+'&limit=1', function (error, response, body) {
+    request('http://119.59.125.170:8080/values?id='+_id31+'&gid='+_gid83+'&limit=1', function (error, response, body) {
           if (!error && response.statusCode == 200) {
             data = JSON.parse(body);
             var thTime = data[0].dt.toLocaleString("en-US", {timeZone: "asia/bangkok"});
